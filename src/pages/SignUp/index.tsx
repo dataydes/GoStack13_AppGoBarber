ff18f9de33b97f4f3a2ac95913d614ee7a79625f
Daniel Ataydes <daniel_ataydes@msn.com>
2020-09-08 17:02:57 -0300

    Criado o botão de next  no teclado mobile

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import { Image, KeyboardAvoidingView, Platform, View, ScrollView } from 'react-native';
+import { Image, KeyboardAvoidingView, Platform, View, ScrollView, TextInput } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
 import Input from '../../components/Input';
@@ -14,6 +14,10 @@ import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
 const SignUp: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
     const navigation = useNavigation();
+
+    const emailInputRef = useRef<TextInput>(null);
+    const passwordInputRef = useRef<TextInput>(null);
+
     return (
         <>
             <KeyboardAvoidingView
@@ -30,9 +34,41 @@ const SignUp: React.FC = () => {
                         <Form ref={formRef} onSubmit={(data) => {
                             console.log(data);
                         }}>
-                            <Input name="name" icon="user" placeholder="Nome" />
-                            <Input name="email" icon="mail" placeholder="E-mail" />
-                            <Input name="password" icon="lock" placeholder="Senha" />
+                            <Input autoCapitalize="words"
+                                name="name"
+                                icon="user"
+                                placeholder="Nome"
+                                returnKeyType="next"
+                                onSubmitEditing={()=>{
+                                    emailInputRef.current?.focus();
+                                }}
+                            />
+
+                            <Input
+                                ref={emailInputRef}
+                                keyboardType="email-address"
+                                autoCorrect={false}
+                                autoCapitalize="none"
+                                name="email"
+                                icon="mail"
+                                placeholder="E-mail"
+                                returnKeyType="next"
+                                onSubmitEditing={()=>{
+                                    passwordInputRef.current?.focus();
+                                }}
+                            />
+
+                            <Input
+                                ref={passwordInputRef}
+                                secureTextEntry
+                                name="password"
+                                icon="lock"
+                                placeholder="Senha"
+                                textContentType="newPassword"
+                                returnKeyType="send"
+                                onSubmitEditing={() => formRef.current?.submitForm()}
+                            />
+
                             <Button onPress={() => formRef.current?.submitForm()}
                             >
                                 Entrar
